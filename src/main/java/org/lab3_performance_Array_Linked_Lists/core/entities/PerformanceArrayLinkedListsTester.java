0d44c3c751e4275047317c2affe5b6dc315eb28b
Nio Homy <NIOHOMY@mail.ru>
2024-04-17 21:31:39 +0300

    PerformanceArrayLinkedListsTester added added ArrayList and LinkedList performance comparison

---
+++ src/main/java/org/lab3_performance_Array_Linked_Lists/core/entities/PerformanceArrayLinkedListsTester.java
@@ -0,0 +1,122 @@
+package org.lab3_performance_Array_Linked_Lists.core.entities;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class PerformanceArrayLinkedListsTester {
+    private ArrayList<Integer> arrayList = new ArrayList<>();
+    private LinkedList<Integer> linkedList = new LinkedList<>();
+
+    private final int MAX_OPERATIONS = 16000;
+
+    public final void testPerformance() {
+        for (int i=1000; i<=MAX_OPERATIONS; i*=2){
+            testAdd(i);
+            testGet(i);
+            testContains(i);
+            testDelete(i);
+            testClear(i);
+        }
+    }
+
+    private  void testDelete(int NUM_OPERATIONS) {
+
+        long arrayListDeleteTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                    arrayList.remove(0);
+            }
+        }, NUM_OPERATIONS);
+
+        long linkedListDeleteTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                    linkedList.removeFirst();
+                }
+        }, NUM_OPERATIONS);
+
+        printResult("Delete  ", arrayListDeleteTime, linkedListDeleteTime, NUM_OPERATIONS);
+    }
+
+    private  void testClear(int NUM_OPERATIONS) {
+
+        for (int i = 0; i < NUM_OPERATIONS; i++) {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+
+        long arrayListClearTime = measureTime(() -> {
+                arrayList.clear();
+        }, NUM_OPERATIONS);
+
+        long linkedListClearTime = measureTime(() -> {
+                linkedList.clear();
+        }, NUM_OPERATIONS);
+
+        printResult("Clear   ", arrayListClearTime, linkedListClearTime, NUM_OPERATIONS);
+    }
+
+    private  void testAdd(int NUM_OPERATIONS) {
+        long arrayListAddTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                    arrayList.add(i);
+            }
+        }, NUM_OPERATIONS);
+
+        long linkedListAddTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                    linkedList.add(i);
+            }
+        }, NUM_OPERATIONS);
+
+        printResult("Add     ", arrayListAddTime, linkedListAddTime, NUM_OPERATIONS);
+    }
+
+
+    private  void testGet(int NUM_OPERATIONS) {
+
+        long arrayListGetTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                    arrayList.get(i);
+                }
+        }, NUM_OPERATIONS);
+
+        long linkedListGetTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                    linkedList.get(i);
+                }
+        }, NUM_OPERATIONS);
+
+        printResult("Get     ", arrayListGetTime, linkedListGetTime, NUM_OPERATIONS);
+    }
+
+    private  void testContains(int NUM_OPERATIONS) {
+
+        long arrayListContainsTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                arrayList.contains(i);
+            }
+        }, NUM_OPERATIONS);
+
+        long linkedListContainsTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                linkedList.contains(i);
+            }
+        }, NUM_OPERATIONS);
+
+        printResult("Contains", arrayListContainsTime, linkedListContainsTime, NUM_OPERATIONS);
+    }
+
+
+    private  long measureTime(Runnable task, int NUM_OPERATIONS) {
+        long startTime = System.nanoTime();
+        task.run();
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / NUM_OPERATIONS;
+    }
+
+    private  void printResult(String operation, long arrayListTime, long linkedListTime, int NUM_OPERATIONS) {
+        System.out.println("Operation\tCount\tArrayList Time (ns)\tLinkedList Time (ns)");
+        System.out.println(operation + "\t" + NUM_OPERATIONS + "\t\t\t" + arrayListTime + "\t\t\t" + linkedListTime);
+        System.out.println(" ");
+    }
+
+}
